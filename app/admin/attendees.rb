f80e6d57102b64f3a23fbce63ee9f10fd3ac57c7
anonymous anonymous <liam.folan@kellogg.northwestern.edu>
2021-12-07 03:46:44 +0000

    Whitelist columns in Attendee for ActiveAdmin

--- app/admin/attendees.rb
+++ app/admin/attendees.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Attendee do
 
+ permit_params :game_id, :attendance_status, :waitlist_status, :player_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
