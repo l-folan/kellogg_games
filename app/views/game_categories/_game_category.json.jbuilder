ec8af69d3acb30ae7b5fdd8fb6f3a1e60d8b1789
anonymous anonymous <liam.folan@kellogg.northwestern.edu>
2021-12-07 03:46:28 +0000

    Generate game_categories resource with ideas:scaffold

---
+++ app/views/game_categories/_game_category.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! game_category, :id, :name, :created_at, :updated_at
+json.url game_category_url(game_category, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :game_categories
   resources :attendees
   resources :messages
   resources :games
