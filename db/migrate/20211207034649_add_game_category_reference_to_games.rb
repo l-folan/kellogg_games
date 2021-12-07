5f88e22e8363984ceb916ac05f65b47860f93401
anonymous anonymous <liam.folan@kellogg.northwestern.edu>
2021-12-07 03:46:49 +0000

    Add foreign key game_category_id reference to games table

---
+++ db/migrate/20211207034649_add_game_category_reference_to_games.rb
@@ -0,0 +1,7 @@
+class AddGameCategoryReferenceToGames < ActiveRecord::Migration[6.0]
+  def change
+    add_foreign_key :games, :game_categories
+    add_index :games, :game_category_id
+    change_column_null :games, :game_category_id, false
+  end
+end
